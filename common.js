b8171884e7c581f8427414e7d6e354d5a118fb5e
Matthew Connolly <miconnolly@upei.ca>
2016-02-14 16:51:44 -0400

    feat:  add roles
    Install the alanning:roles package.  Add roles to user schema as per
    the collection2 example. Add role names as constants to
    /lib/constants.js to avoid using strings in any calls to methods
    involving roles.
    
    Closes #28

--- common.js
+++ common.js
@@ -89,6 +89,19 @@ Schema.Users = new SimpleSchema({
         optional: true,
         blackbox: true
     },
+    // Add `roles` to your schema if you use the meteor-roles package.
+    // Option 1: Object type
+    // If you specify that type as Object, you must also specify the
+    // `Roles.GLOBAL_GROUP` group whenever you add a user to a role.
+    // Example:
+    // Roles.addUsersToRoles(userId, ["admin"], Roles.GLOBAL_GROUP);
+    // You can't mix and match adding with and without a group since
+    // you will fail validation in some cases.
+    roles: {
+        type: Object,
+        optional: true,
+        blackbox: true
+    },
     // In order to avoid an 'Exception in setInterval callback' from Meteor
     heartbeat: {
         type: Date,
--- readme.txt
+++ readme.txt
@@ -2,6 +2,7 @@ Plugins for meteor used so far:
 
 accounts-password
 aldeed:collection2 - Schemas
+alanning:roles
 angular
 dotansimha:accounts-ui-angular
 email
