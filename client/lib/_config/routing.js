74f6ce7d90de0b12e635c9601cf6d869e3bac7d9
Matthew Connolly <miconnolly@upei.ca>
2016-03-14 22:24:43 -0300

    updated mockup integration for reference
    again, do not merge

--- client/lib/_config/routing.js
+++ client/lib/_config/routing.js
@@ -5,9 +5,109 @@
     $locationProvider.html5Mode(true);
     
     $stateProvider
+      .state('answerQuestion', {
+        url: '/answer-question/:questionId',
+        templateUrl: 'client/templates/routes/answerQuestion.html',
+        data: {
+          rule: function () {
+            if (!Meteor.userId() ||
+                !Meteor.user().emails[0].verified)
+            {
+              return {
+                to: 'home',
+                params: {}
+              }
+            }
+          }
+        }
+      })
+      .state('createGroup', {
+        url: '/create-group',
+        templateUrl: 'client/templates/routes/createGroup.html',
+        data: {
+          rule: function () {
+            if (!Meteor.userId() ||
+                !Meteor.user().emails[0].verified ||
+                !Roles.userIsInRole(Meteor.userId(), PROFESSOR_ROLE, Roles.GLOBAL_GROUP))
+            {
+              return {
+                to: 'home',
+                params: {}
+              }
+            }
+          }
+        }
+      })
+      .state('createQuestion', {
+        url: '/create-question',
+        templateUrl: 'client/templates/routes/createQuestion.html',
+        data: {
+          rule: function () {
+            if (!Meteor.userId() ||
+                !Meteor.user().emails[0].verified ||
+                !Roles.userIsInRole(Meteor.userId(), PROFESSOR_ROLE, Roles.GLOBAL_GROUP))
+            {
+              return {
+                to: 'home',
+                params: {}
+              }
+            }
+          }
+        }
+      })
       .state('home', {
         url: '/',
-        templateUrl: 'client/templates/routes/home.html'
+        templateUrl: 'client/templates/routes/home.html',
+        data: {
+          rule: function () {
+            if (!Meteor.userId() ||
+                !Meteor.user().emails[0].verified)
+            {
+              return {
+                to: 'welcome',
+                params: {}
+              }
+            }
+          }
+        }
+      })
+      .state('updateQuestion', {
+        url: '/update-question/:questionId',
+        templateUrl: 'client/templates/routes/updateQuestion.html',
+        data: {
+          rule: function () {
+            if (!Meteor.userId() ||
+                !Meteor.user().emails[0].verified ||
+                !Roles.userIsInRole(Meteor.userId(), PROFESSOR_ROLE, Roles.GLOBAL_GROUP))
+            {
+              return {
+                to: 'home',
+                params: {}
+              }
+            }
+          }
+        }
+      })
+      .state('updateGroup', {
+        url: '/update-group/:groupId',
+        templateUrl: 'client/templates/routes/updateGroup.html',
+        data: {
+          rule: function () {
+            if (!Meteor.userId() ||
+                !Meteor.user().emails[0].verified ||
+                !Roles.userIsInRole(Meteor.userId(), PROFESSOR_ROLE, Roles.GLOBAL_GROUP))
+            {
+              return {
+                to: 'home',
+                params: {}
+              }
+            }
+          }
+        }
+      })
+      .state('welcome', {
+        url: '/welcome',
+        templateUrl: 'client/templates/routes/welcome.html'
       });
       
     $urlRouterProvider.otherwise('/');
