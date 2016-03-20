c6b9b2c46c84079325155214bf4d9f6670c27fb7
Matthew Connolly <miconnolly@upei.ca>
2016-03-20 15:51:47 -0300

    feat:  add update group template and route
    Add template, route, and rules for the update group view.
    
    All tests pass.
    
    Closes #63.

--- client/lib/_config/routing.js
+++ client/lib/_config/routing.js
@@ -8,6 +8,23 @@
       .state('home', {
         url: '/',
         templateUrl: 'client/templates/routes/home.html'
+      })
+      .state('updateGroup', {
+        url: '/update-group/:groupId',
+        templateUrl: 'client/templates/routes/updateGroup.html',
+        data: {
+          rule: function () {
+            if (!Meteor.userId() ||
+                !Meteor.user().emails[0].verified ||
+                !Roles.userIsInRole(Meteor.userId(), PROFESSOR_ROLE, Roles.GLOBAL_GROUP))
+            {
+              return {
+                to: 'home',
+                params: {}
+              }
+            }
+          }
+        }
       });
       
     $urlRouterProvider.otherwise('/');
