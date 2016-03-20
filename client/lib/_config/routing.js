25cdeeba91e2185bd2d5e6f7559bf32310a5e3f4
Matthew Connolly <miconnolly@upei.ca>
2016-03-20 15:27:40 -0300

    feat:  add answer question template and route
    Add template, route and rules for the answer question view.
    
    All tests pass
    
    Closes #60.

--- client/lib/_config/routing.js
+++ client/lib/_config/routing.js
@@ -5,6 +5,22 @@
     $locationProvider.html5Mode(true);
     
     $stateProvider
+      .state('answerQuestion', {
+        url: '/answer-question/:questionId',
+        templateUrl: 'client/templates/routes/answerQuestion.html',
+        data: {
+          rule: function () {
+            if (!Meteor.userId() ||
+                !Meteor.user().emails[0].verified)
+            {
+              return {
+                to: 'home',
+                params: {}
+              }
+            }
+          }
+        }
+      })
       .state('home', {
         url: '/',
         templateUrl: 'client/templates/routes/home.html'
