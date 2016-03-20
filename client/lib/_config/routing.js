2bc177255979e88ea6bab1f0bf2491a3de87d9f9
Matthew Connolly <miconnolly@upei.ca>
2016-03-20 15:37:06 -0300

    feat:  add create group template and route
    Add template, route, and rules for the create group view.
    
    All tests pass.
    
    Closes #52.

--- client/lib/_config/routing.js
+++ client/lib/_config/routing.js
@@ -5,6 +5,23 @@
     $locationProvider.html5Mode(true);
     
     $stateProvider
+      .state('createGroup', {
+        url: '/create-group',
+        templateUrl: 'client/templates/routes/createGroup.html',
+        data: {
+          rule: function () {
+            if (!Meteor.userId() ||
+                !Meteor.user().emails[0].verified ||
+                !Roles.userIsInRole(Meteor.userId(), PROFESSOR_ROLE, Roles.GLOBAL_GROUP))
+            {
+              return {
+                to: 'home',
+                params: {}
+              }
+            }
+          }
+        }
+      })
       .state('home', {
         url: '/',
         templateUrl: 'client/templates/routes/home.html'
