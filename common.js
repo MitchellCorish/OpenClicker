1ba2b8d3cbd4f57eb3e64153252fb78f1b7e1d9b
Matthew Connolly <miconnolly@upei.ca>
2016-04-03 18:50:38 -0300

    fix:  add comma needed from merge
    Add comma to common.js before deleteUserFromGroup().
    
    All tests pass.

--- common.js
+++ common.js
@@ -472,7 +472,7 @@ Meteor.methods({
     });
 
     return true;
-  }
+  },
   deleteUserFromGroup: function (userId, groupId) {
     MethodHelpers.checkUserLoggedIn();
     MethodHelpers.checkVerifiedUser();
